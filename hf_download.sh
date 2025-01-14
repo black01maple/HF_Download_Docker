2337e3dcefd074f228a6cbf17d0239fdc2bad0b5
black01maple <90243329+black01maple@users.noreply.github.com>
2025-01-14 15:04:33 +0800

    Add files via upload
    fix bugs

--- hf_download.sh
+++ hf_download.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# 检查是否找到宿主机挂载点
-mount_info=$(grep " $container_path " /proc/self/mountinfo)
-if [ -z "$mount_info" ]; then
-    echo "没有挂载宿主机模型下载目录，下载终止"
-    echo "请通过 docker run -v 我的下载目录:/opt/saved_model_parameters 来挂载路径"
-    exit 1
-else
-    # 提取宿主机路径
-    host_path=$(echo $mount_info | awk '{print $5}')
-    # 输出宿主机路径
-    echo "宿主机模型下载目录: $host_path"
-fi
-
 # 检查环境变量model_name是否存在
 if [ -z "$model_name" ]; then
     echo "模型名称model_name未设置(环境变量)，下载终止"
